6dff5702a47fc6ae35091e90a96ef77491e3b8bc
udkfer <udkfer@gmail.com>
2024-08-31 20:03:28 -0300

    feat: create routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { PostImageDTO } from './DTOs/PostImageDTO';
+import { GetMeasuresDTO } from './DTOs/GetMeasuresDTO';
+import { PatchConfirmDTO } from './DTOs/PatchConfirmDTO';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/upload')
+  uploadFile(@Body() postImageDTO: PostImageDTO) {
+    return this.appService.postImage(postImageDTO);
+  }
+
+  @Patch('/confirm')
+  confirmMeasure(@Body() patchConfirmDTO: PatchConfirmDTO) {
+    return this.appService.patchConfirm(patchConfirmDTO);
+  }
+
+  @Get('/:customer_code/list')
+  getMeasures(
+    @Param() { customer_code }: GetMeasuresDTO,
+    @Query('measure_type') measure_type: string,
+  ) {
+    return this.appService.getMeasures(customer_code, measure_type);
   }
 }
