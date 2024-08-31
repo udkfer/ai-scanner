0cadc17b037173f44f413f2b6d0692b3f62bcf90
udkfer <udkfer@gmail.com>
2024-08-31 20:01:23 -0300

    feat: config class validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        return new BadRequestException({
+          error_code: 'INVALID_DATA',
+          error_message: errors.map((error) => {
+            return {
+              property: error.property,
+              constraints: error.constraints,
+            };
+          }),
+        });
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
